edc467a81f3012edacbd40cb1f2f69df0c846930
TheNumberGuy <tiagorocha1515@gmail.com>
2021-02-26 21:54:02 +0000

    feat: component challenges completed complete

---
+++ src/components/CompletedChallenges.tsx
@@ -0,0 +1,10 @@
+import styles from '../styles/components/CompletedChallenges.module.css';
+
+export function CompletedChallenges() {
+  return (
+    <div className={styles.completedChallengesContainer}>
+      <span>Completed Challenges</span>
+      <span>15</span>
+    </div>
+  );
+}
