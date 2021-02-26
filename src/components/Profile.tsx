408b5f23c1341125647262049ad94087c8069824
TheNumberGuy <tiagorocha1515@gmail.com>
2021-02-26 21:42:40 +0000

    feat: profile component complete

---
+++ src/components/Profile.tsx
@@ -0,0 +1,14 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+  return <div className={styles.profileContainer}>
+    <img src="https://github.com/TiagoR15.png" alt="Tiago Rocha"/>
+    <div>
+      <strong>Tiago Rocha</strong>
+      <p>
+        <img src="icons/level.svg" alt="Level"/>
+        Level 1
+      </p>
+    </div>
+  </div>;
+}
