05900c4330a36df3612c1418c4fc0672a73e389c
TheNumberGuy <tiagorocha1515@gmail.com>
2021-03-06 14:38:49 +0000

    feat: challengesContext added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import "../styles/global.css";
 
+import { ChallengesContext, ChallangesProvider } from '../contexts/ChallengesContext';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+
+  return (
+    <ChallangesProvider>
+      <Component {...pageProps} />
+    </ChallangesProvider>
+  );
 }
 
 export default MyApp;
